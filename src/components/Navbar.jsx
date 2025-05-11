7ec089be9313441121421a7eb13890bbaf9e1c42
Andrew Laryea <laryeaandrew0001@gmail.com>
2025-05-11 10:57:26 +0000

    Header and Navbar Section done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,38 @@
 import React from "react";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 
 const Navbar = ({ navOpen }) => {
   const lastActiveLink = useRef(null);
   const activeBox = useRef(null);
+
+  const initActiveBox = () => {
+    // console.log(lastActiveLink.current);
+    // console.log(activeBox.current);
+    // activeBox.current.style.top = "4px";
+    activeBox.current.style.top = lastActiveLink.current.offsetTop + "px";
+    activeBox.current.style.left = lastActiveLink.current.offsetLeft + "px";
+    activeBox.current.style.width = lastActiveLink.current.offsetWidth + "px";
+    activeBox.current.style.height = lastActiveLink.current.offsetHeight + "px";
+  };
+
+  const currentActiveLink = (e) => {
+    lastActiveLink.current?.classList.remove("active");
+    e.target.classList.add("active");
+    lastActiveLink.current = e.target;
+
+    // activeBox.current.style.top = "4px";
+    activeBox.current.style.top = e.target.offsetTop + "px";
+    activeBox.current.style.left = e.target.offsetLeft + "px";
+    activeBox.current.style.width = e.target.offsetWidth + "px";
+    activeBox.current.style.height = e.target.offsetHeight + "px";
+  };
+
+  useEffect(initActiveBox, []);
+  window.addEventListener("resize", initActiveBox); // Recalculate the position of the active box on window resize
+  window.addEventListener("load", initActiveBox); // Recalculate the position of the active box on page load
+  window.addEventListener("scroll", initActiveBox); // Recalculate the position of the active box on scroll
+  window.addEventListener("click", initActiveBox); // Recalculate the position of the active box on click
 
   const navItems = [
     {
@@ -37,7 +65,13 @@ const Navbar = ({ navOpen }) => {
   return (
     <nav className={"navbar" + (navOpen ? " active" : "")}>
       {navItems.map(({ label, link, className, ref }, key) => (
-        <a href={link} className={className} key={key} ref={ref} onClick={null}>
+        <a
+          href={link}
+          className={className}
+          key={key}
+          ref={ref}
+          onClick={currentActiveLink}
+        >
           {label}
         </a>
       ))}
